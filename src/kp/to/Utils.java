15ea76b07eae6dc1b65786583be76c2c21a567ea
User Two <UserTwo@domain.com>
2015-12-08 17:26:13 +0200

    Utils for rounding

---
+++ src/kp/to/Utils.java
@@ -0,0 +1,12 @@
+package kp.to;
+
+/**
+ * Created by inf106580 on 2015-12-08.
+ */
+public class Utils {
+
+    public static int round(double value) {
+        return (int) Math.floor((value) + 0.5);
+    }
+
+}
