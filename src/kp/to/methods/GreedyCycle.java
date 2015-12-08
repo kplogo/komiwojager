2415d394ce25c1b123e3f49e304024228becc30f
Mikolaj <mikolaj@mail.com>
2015-12-08 16:37:29 +0100

    RoundResult

--- src/kp/to/methods/GreedyCycle.java
+++ src/kp/to/methods/GreedyCycle.java
@@ -2,15 +2,23 @@ package kp.to.methods;
 
 import kp.to.model.Point;
 import kp.to.model.Result;
+import kp.to.model.RoundResult;
 
 import java.util.List;
 
-/**
- * Created by student on 2015-12-08.
- */
 public class GreedyCycle implements Algorithm {
     @Override
     public Result run(List<Point> pointList) {
+        Result result = new Result();
+        for (int i = 0; i < pointList.size(); i++) {
+            RoundResult roundResult = calculate(pointList,i);
+            result.addResult(roundResult);
+        }
+        return null;
+    }
+
+    private RoundResult calculate(List<Point> pointList, int startPoint) {
+
         return null;
     }
 }
---
+++ src/kp/to/model/RoundResult.java
@@ -0,0 +1,9 @@
+package kp.to.model;
+
+/**
+ * Wynik dla pojedyñczej iteracji (pojedynczy wierzcho³ek startowy)
+ */
+public class RoundResult {
+    private int startPointIndex;
+
+}
