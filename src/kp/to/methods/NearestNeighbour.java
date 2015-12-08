6fe04cd3f0c1e4fe8e6eeaeb7f44021d05fac40b
User Two <UserTwo@domain.com>
2015-12-08 18:00:12 +0200

    NN implemented (I hope)

--- src/kp/to/methods/NearestNeighbour.java
+++ src/kp/to/methods/NearestNeighbour.java
@@ -1,7 +1,64 @@
 package kp.to.methods;
 
+import kp.to.Utils;
+import kp.to.model.Point;
+import kp.to.model.Result;
+import kp.to.model.RoundResult;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 /**
  * Created by student on 2015-12-08.
  */
-public class NearestNeighbour {
+public class NearestNeighbour implements Algorithm {
+
+    private final Random random;
+    private List<Point> pointList;
+    public NearestNeighbour() {
+        this.random = new Random(System.currentTimeMillis());
+    }
+
+    private Point getNearestPoint(Point reference) {
+        Point nearestPoint = null;
+        int minDist = Integer.MAX_VALUE;
+        for (Point p : pointList) {
+            int dist = Utils.length(reference, p);
+            if (dist < minDist || (dist == minDist && random.nextBoolean())) {
+                minDist = dist;
+                nearestPoint = p;
+            }
+        }
+        return nearestPoint;
+    }
+
+    @Override
+    public Result run(List<Point> pointList) {
+        Result result = new Result();
+        for (Point startPoint : pointList) {
+            this.pointList = new ArrayList<>(pointList);
+            RoundResult r = calculate(startPoint);
+            result.addResult(r);
+        }
+        return result;
+    }
+
+    private RoundResult calculate(Point startPoint) {
+        RoundResult result = new RoundResult(startPoint.getLabel());
+        Point previouslyAdded = startPoint;
+        result.add(startPoint);
+        pointList.remove(startPoint);
+        int routeLength = 0;
+        while (!pointList.isEmpty()) {
+            Point nearest = getNearestPoint(previouslyAdded);
+            pointList.remove(nearest);
+            result.add(nearest);
+            routeLength += Utils.length(previouslyAdded, nearest);
+            previouslyAdded = nearest;
+        }
+        routeLength += Utils.length(previouslyAdded, startPoint);
+        result.setRouteLength(routeLength);
+        return result;
+    }
 }
--- src/kp/to/model/RoundResult.java
+++ src/kp/to/model/RoundResult.java
@@ -5,8 +5,17 @@ package kp.to.model;
  */
 public class RoundResult {
     private int startPointIndex;
+    private int routelength;
 
     public RoundResult(int startPoint) {
+        this.startPointIndex = startPoint;
+    }
 
+    public void add(Point p) {
+        //TODO: implement
+    }
+
+    public void setRouteLength(int length) {
+        this.routelength = length;
     }
 }
