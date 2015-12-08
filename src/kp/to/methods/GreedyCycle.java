8f09ca6ed3e970cdad7053fb09dff70fe0dee46b
Mikolaj <mikolaj@mail.com>
2015-12-08 17:21:18 +0100

    Greedy

--- src/kp/to/methods/GreedyCycle.java
+++ src/kp/to/methods/GreedyCycle.java
@@ -22,7 +22,7 @@ public class GreedyCycle implements Algorithm {
     private RoundResult calculate(List<Point> pointList, int startPointIndex) {
         RoundResult roundResult = new RoundResult(pointList.get(startPointIndex));
         int nearestPointId = 0;
-        while (nearestPointId != -1) {
+        while (roundResult.size() != pointList.size()) {
             nearestPointId = getNearestPoint(pointList, roundResult);
         }
         return roundResult;
@@ -38,21 +38,11 @@ public class GreedyCycle implements Algorithm {
                 continue;
             }
             int minForPoint = Integer.MAX_VALUE;
-            int minPositionForPoint = -1;
+            int minPositionForPoint = 0;
 
             for (int j = 0; j < startPoint.size(); j++) {
-                int length = 0;
-                for (int k = 1; k < startPoint.size(); k++) {
-                    Point prevPoint = startPoint.get(k - 1);
-                    Point actualPoint = startPoint.get(k);
-
-                    if (j == k) {
-                        length += Utils.length(prevPoint, point);
-                        length += Utils.length(point, actualPoint);
-                    } else {
-                        length += Utils.length(prevPoint, actualPoint);
-                    }
-                }
+                int length = calculateLength(startPoint, point, j);
+                System.out.println(length);
                 if (minForPoint > length) {
                     minForPoint = length;
                     minPositionForPoint = i;
@@ -68,11 +58,29 @@ public class GreedyCycle implements Algorithm {
         System.out.println(minId);
         System.out.println(minPosition);
         Point point = pointList.get(minId);
-        if (minPosition == -1) {
-            startPoint.add(point);
-        } else {
-            startPoint.add(minPosition, point);
-        }
+        startPoint.add(minPosition, point);
+
         return minId;
+    }
+
+    private int calculateLength(RoundResult startPoint, Point point, int newPosition) {
+        int length = 0;
+        for (int k = 0; k < startPoint.size(); k++) {
+            Point prevPoint = startPoint.get(k);
+            Point actualPoint;
+            if (k == startPoint.size() - 1) {
+                actualPoint = point;
+                newPosition = -1;
+            } else {
+                actualPoint = startPoint.get(k + 1);
+            }
+            if (newPosition == k) {
+                length += Utils.length(prevPoint, point);
+                length += Utils.length(point, actualPoint);
+            } else {
+                length += Utils.length(prevPoint, actualPoint);
+            }
+        }
+        return length;
     }
 }
