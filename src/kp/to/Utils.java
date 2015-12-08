ad5d95fe83c5a7e9dd5b047d0bf906a56b07027f
User Two <UserTwo@domain.com>
2015-12-08 17:34:34 +0200

    Utils for rounding2

--- src/kp/to/Utils.java
+++ src/kp/to/Utils.java
@@ -1,5 +1,7 @@
 package kp.to;
 
+import kp.to.model.Point;
+
 /**
  * Created by inf106580 on 2015-12-08.
  */
@@ -7,6 +9,10 @@ public class Utils {
 
     public static int round(double value) {
         return (int) Math.floor((value) + 0.5);
+    }
+
+    public static double length(Point p1, Point p2) {
+        return round(Math.sqrt((Math.pow((p1.getX()-p2.getX()),2) + Math.pow((p1.getY()-p2.getY()),2))));
     }
 
 }
