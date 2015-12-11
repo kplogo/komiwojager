c60bfdd673159cf4837b30a8eb32c3d38be79bf0
Pawlak <Krzysztof.Pawlak@wolterskluwer.com>
2015-12-11 10:18:22 +0100

    grasp zachlanny losowy

--- src/kp/to/methods/GraspGreedy.java
+++ src/kp/to/methods/GraspGreedy.java
@@ -8,17 +8,15 @@ import kp.to.model.RoundResult;
 public class GraspGreedy extends Grasp {
     @Override
     protected RoundResult getNeighbour(RoundResult solution) {
-            RoundResult newSolution;
-            for (int i = 0; i < solution.size(); i++) {
-                for (int j = i; j < solution.size(); j++) {
-                    if (isNewSolutionCostBetter(solution, i, j)>0) {
-                        newSolution = generateSolution(solution, i, j);
-                        return newSolution;
-                    }
+        RoundResult newSolution;
+        for (int i = 0; i < solution.size(); i++) {
+            for (int j = i; j < solution.size(); j++) {
+                if (isNewSolutionCostBetter(solution, i, j) > 0) {
+                    newSolution = generateSolution(solution, i, j);
+                    return newSolution;
                 }
-
             }
-            return null;
-
+        }
+        return null;
     }
 }
