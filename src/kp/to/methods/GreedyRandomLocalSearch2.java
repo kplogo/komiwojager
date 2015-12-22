4e139f08345de1437a6b8a5a99fad7ec35167208
User Two <UserTwo@domain.com>
2015-12-22 18:10:53 +0200

    Minor changes, greedyrandom other version

---
+++ src/kp/to/methods/GreedyRandomLocalSearch2.java
@@ -0,0 +1,36 @@
+package kp.to.methods;
+
+import kp.to.methods.LocalSearch;
+import kp.to.methods.localsearch.type.LocalSearchType;
+import kp.to.model.RoundResult;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Created by inf106580 on 2015-12-22.
+ */
+public class GreedyRandomLocalSearch2 extends LocalSearch{
+    public GreedyRandomLocalSearch2(LocalSearchType type) {
+        super(type);
+    }
+
+    @Override
+    protected RoundResult getNeighbour(RoundResult solution) {
+        List<Integer> order = new ArrayList<>();
+        for (int i = 0; i < solution.size(); i++) {
+            order.add(i);
+        }
+        Collections.shuffle(order);
+        for (int i = 0; i < order.size(); i++) {
+            for (int j = i+1; j < order.size(); j++) {
+                if (isNewSolutionCostBetter(solution, order.get(i), order.get(j)) > 0) {
+                    return generateSolution(solution, order.get(i), order.get(j));
+                }
+            }
+
+        }
+        return null;
+    }
+}
